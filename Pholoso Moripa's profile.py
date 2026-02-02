3a36adc60cc5b44388c93a255003566fef1b0143
Moripa-Mac <pholosomoripa6@gmail.com>
2026-02-02 11:40:34 +0200

    Add files via upload

---
+++ Pholoso Moripa's profile.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  2 11:30:28 2026
+
+@author: Pholoso
+"""
+
+import streamlit as st
+
+st.set_page_config(page_title="Researcher Profile", layout="centered")
+
+st.title("Researcher Profile")
+
+st.header("Pholoso Robby Moripa")
+st.write("MSc Student | Bioinformatics & Aquaculture Research")
+
+st.subheader("Institution")
+st.write("University of Limpopo")
+
+st.subheader("Research Interests")
+st.markdown("""
+- Gut microbiota in aquaculture species  
+- 16S rRNA gene sequencing  
+- Bioinformatics analysis  
+- Fish health & sustainability
+- Fish farming  
+""")
+
+st.subheader("Current Research")
+st.write("""
+Impact of seasonal variations and ontogenic shifts on gut microbiota composition
+and diversity of *Oreochromis mossambicus*.
+""")
+
+st.subheader("🧪 Skills & Tools")
+st.markdown("""
+- Python  
+- R  
+- Streamlit  
+- QIIME2  
+- Linux (Bash)
+""")
+
+st.subheader("Contact")
+st.write("Email: pholosomoripa6@email.com")
